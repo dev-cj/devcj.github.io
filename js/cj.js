2e12dc6dba731d983d06bc808bb9fac70c0c925b
dev-cj <devcj.in@gmail.com>
2020-03-17 20:50:56 +0530

    PWA added

--- js/cj.js
+++ js/cj.js
@@ -1,12 +1,8 @@
 // Sending request to Your Day to start server
 
-;(async () => {
-  const blah = fetch('https://bit.ly/fullstackYourDay/')
-  console.log('blah :', blah)
-  if (blah) {
-    // console.clear()
-    console.log('00000000 :', 00000000)
-  }
-
-  // setTimeout(() => {}, 1000)
+;(() => {
+  fetch('https://bit.ly/fullstackYourDay/')
+  setTimeout(() => {
+    console.clear()
+  }, 1000)
 })()
--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,21 @@
 const cacheName = 'devcj-service-worker'
-// const filesToCache = [
-//   '/fonts/',
-//   '/images/',
-//   '/',
-//   '/js/demo.js',
-//   '/js/jquery.particlebackground.js',
-//   '/pages/'
-// ]
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', e => {
-  console.log('Inside instll handle :', e)
-  // e.waitUntil(self.skipWaiting())
+  console.log('Inside install handle ')
 })
 
-/* Serve cached content when offline */
-// self.addEventListener('fetch', function(e) {
-//   e.respondWith(
-//     caches.match(e.request).then(function(response) {
-//       return response || fetch(e.request)
-//     })
-//   )
-// })
 self.addEventListener('message', event => {
-  console.log('eventaaaaaaa :', event.data.type)
   if (event.data.type === 'CACHE_URLS') {
     event.waitUntil(
       caches.open(cacheName).then(cache => {
-        console.log('cache :', cache)
         return cache.addAll(event.data.payload)
       })
     )
   }
 })
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(response => {
       return response || fetch(e.request)
     })
   )
