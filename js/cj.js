c232b891e48b05da494e69f26f581b1c472bb4d2
dev-cj <devcj.in@gmail.com>
2023-01-06 20:29:59 +0530

    update 23

--- js/cj.js
+++ js/cj.js
@@ -1,8 +1,29 @@
 // Sending request to Your Day to start server
 
-;(() => {
-  fetch('http://bit.ly/fullstackYourDay/')
-  setTimeout(() => {
-    console.clear()
-  }, 1000)
-})()
+// ;(() => {
+//   fetch('http://bit.ly/fullstackYourDay/')
+//   setTimeout(() => {
+//     console.clear()
+//   }, 1000)
+// })()
+
+let closeBtn = document.querySelector('.close-carousel-button')
+let viewImage = document.querySelector('.uniping-view')
+let carouselWrapper = document.querySelector('.flickity-carousel')
+let fstate = 0
+
+function toggleCarousel() {
+  if (fstate == 0) {
+    // close
+    carouselWrapper.style.opacity = '100'
+    carouselWrapper.style.zIndex = '99999'
+    fstate = 1
+  } else {
+    carouselWrapper.style.opacity = '0'
+    carouselWrapper.style.zIndex = '-1'
+    fstate = 0
+  }
+}
+
+viewImage.addEventListener('click', toggleCarousel)
+closeBtn.addEventListener('click', toggleCarousel)
